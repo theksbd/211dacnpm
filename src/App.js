e6ec71e9780b90c746689357b9b25d08e931fe17
TienMinh <tienminh0801@gmail.com>
2021-11-02 11:09:55 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import ProductList from "./component/ProductList";
 import Login from "./component/Login"
 
 export default function App() {
-  // return <ProductList />
-  return <Login />
+  return <ProductList /> 
+  // return <Login />
 }
