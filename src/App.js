1d2281c78d8db16acac9492c0a9ecd2c718be9e5
theksbd <93083446+theksbd@users.noreply.github.com>
2021-11-03 21:16:48 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ProductList from "./components/ProductList";
-import Login from "./components/Login";
+//import Login from "./components/Login";
 import Header from "./components/Header"
 import Footer from "./components/Footer";
+import OrderAdmin from "./components/OrderAdmin";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
@@ -12,12 +14,15 @@ function App() {
       <div className="row">
         <Header />
       </div>
-      <div className="row">
+      {/* <div className="row">
         <ProductList />
-      </div>
+      </div> */}
       {/* <div className="row">
         <Login />
       </div> */}
+      <div className="row">
+        <OrderAdmin />
+      </div>
       <div className="row">
         <Footer />
       </div>
