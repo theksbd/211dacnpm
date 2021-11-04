81b14c03fb9b1b464eda77d7e6dc9ec6e4771d45
TienMinh <tienminh0801@gmail.com>
2021-11-04 20:31:47 +0700

    Check

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import ProductList from "./component/ProductList";
 import Login from "./component/Login"
+import EditItem from "./pages/EditItem"
+import AddItem from "./pages/AddItem"
 
 export default function App() {
-  return <ProductList /> 
+  // return <ProductList /> 
   // return <Login />
+  // return <EditItem /> 
+  return <AddItem /> 
+
 }
