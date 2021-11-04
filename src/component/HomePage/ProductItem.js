004ad8d9251f5e163b26210038d5c190f14613bc
TienMinh <tienminh0801@gmail.com>
2021-11-05 01:58:09 +0700

    Update ProductList

--- src/component/HomePage/ProductItem.js
+++ src/component/HomePage/ProductItem.js
@@ -2,32 +2,33 @@ export default function ProductItem(props) {
 
 
     return (
-      <div class="col-md-3 col-lg col-xl my-5">
-        <div class="card shadow"
+      <div className="col-md-3 col-lg col-xl my-5">
+        <div className="card shadow"
           style={{
             width: '100%',
             border: '0.5px solid #C4C4C4', borderRadius: '12px', cursor: 'pointer',
             '&:hover': {
               transform: 'scale(1.1)',
-            }
+            },
+            maxWidth: '302px'
           }}
           onClick={() => 1}>
-          <img src={props.product.image} class="card-img-top " alt="..." style={{ width: '90%', borderRadius: '10%' }} />
-          <div class="card-body">
-            <h5 class="card-title" style={{minHeight:'48px'}}>{props.product.productName}</h5>
-            <div class='row' style={{ paddingRight: '16px', paddingLeft: '4px' }}>
-              <div class='col-md col-lg col-xl float-start m-2 rounded text-center' style={{ backgroundColor: '#C4C4C4', width: '100%' }}>
+          <img src={props.product.image} className="card-img-top " alt="..." style={{ width: '90%', borderRadius: '10%'}} />
+          <div className="card-body">
+            <h5 className="card-title" style={{minHeight:'48px'}}>{props.product.productName}</h5>
+            <div className='row' style={{ paddingRight: '4px', paddingLeft: '4px' }}>
+              <div className='col-md col-lg col-xl float-start m-2 rounded text-center' style={{ backgroundColor: '#C4C4C4', maxWidth: '104px' }}>
                 {props.product.specifications.screen} inches
               </div>
-              {/* <div class='col-md m-2 rounded' style={{ backgroundColor: '#C4C4C4' }}>
+              <div className='col-md col-lg col-xl m-2 rounded' style={{ backgroundColor: '#C4C4C4', maxWidth: '62px' }}>
                 {props.product.specifications.ram}GB
-              </div> */}
-              <div class='col-md col-lg col-xl m-2 rounded text-center' style={{ backgroundColor: '#C4C4C4' }}>
+              </div>
+              <div className='col-md col-lg col-xl m-2 rounded text-center' style={{ backgroundColor: '#C4C4C4', maxWidth: '72px' }}>
                 {props.product.specifications.rom}GB
               </div>
             </div>
-            <p class="text-danger h1">{props.product.newPrice}</p>
-            <p class="text-muted h3">{props.product.oldPrice}</p>
+            <p className="text-danger h1">{props.product.newPrice}</p>
+            <p className="text-muted h3 text-decoration-line-through">{props.product.oldPrice}</p>
           </div>
         </div>
       </div>
