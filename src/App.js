55e58f382274d5eb26ea9dc6e0579d870345a023
quan-nguyen-thuc06 <quannguyenthuc06@gmail.com>
2021-11-05 14:46:09 +0700

    Update AddItem.js, EditItem.css, EditItem.js and add ManagerItem.js/css Admin.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import ProductList from "./component/ProductList";
 import Login from "./component/Login"
-
-export default function App() {
-  return <ProductList /> 
-  // return <Login />
+import EditItem from "./pages/EditItem";
+import AddItem from "./pages/AddItem";
+import Admin from "./pages/Admin";
+import ManagerItem from "./pages/ManagerItem";
+import React, {Component}from 'react'
+import axios from 'axios';
+// import EditItem from "./component/test";
+import ImageUploadPreviewComponent from './component/ImageUploadPreviewComponent'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+export default class App extends Component {
+  render(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<EditItem/>} />
+        <Route path="editItem/*" element={<EditItem />} />
+        <Route path="addItem/*" element={<AddItem />} />
+        <Route path="admin/*" element={<Admin />} />
+        <Route path="managerItem/*" element={<ManagerItem />} />
+      </Routes>
+    </BrowserRouter>
+  );
+  }
 }
+
